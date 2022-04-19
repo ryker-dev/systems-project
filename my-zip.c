80d5638c74e433447feb2afab63d1a2936ff5e83
ryker-dev <pizzuti.dev@gmail.com>
2022-04-19 21:27:58 +0300

    Start for zip

--- my-zip.c
+++ my-zip.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+
+#define BUFFERLEN 255
+
+//char BUFFER[BUFFERLEN];
+
+FILE *openFile(char name[20], char mode[20]) {
+    FILE *fp = fopen(name, mode);
+
+    if (fp == NULL) {
+        printf("cannot open file\n");
+        exit(1);
+    }
+
+    return fp;
+}
+
+/*
+char* RLE() {
+
+} */
+
+/* setmode(fileno(stdout), O_BINARY) */
 
 int main(int argc, char const *argv[])
 {
-    printf("Test!\n");
+    FILE *in = openFile("test.txt", "r");
+    //FILE *out = openFile("output.txt", "w");
+    char c, curChar;
+    char *buff[5];
+    int counter = 0;
+    while ((c = getc(in)) != EOF) {
+        if (counter < 1 || c != curChar) {
+            if (counter > 0 ) {
+                fwrite(counter, 1, sizeof(c), stdout);
+            }
+            counter = 0;
+            curChar = c;
+        } else {
+            counter++;
+        }
+    };
     return 0;
 }

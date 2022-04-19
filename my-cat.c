3cb68035bbc7625be067f15c3655eb7cd9a136d1
JuhoSpoof <juho.spoof@kolumbus.fi>
2022-04-19 23:03:52 +0300

    vissin done

--- my-cat.c
+++ my-cat.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, char const *argv[]){
 
-    printf("--alku testi--\n");   //test 1
+    char buffer[10];
+    char file[256];
+    int i=1;
+    while (i < argc){
+        strcpy(file, argv[i]);
 
-    int size = 10;
-    //int size;
-    char line[10];
-    FILE *fp = fopen("main.c", "r");
+        FILE *fp = fopen(file, "r");
 
-    if (fp == NULL) {
-        printf("cannot open file\n");
-        exit(1);
+        if (fp == NULL) {
+            printf("my-cat: cannot open file\n");
+            exit(1);
+        }
+
+        while (fgets(buffer,sizeof(buffer),fp) != NULL){
+            printf("%s" ,buffer);
+        }
+
+        fclose(fp); 
+        i++;
     }
 
-    while (fgets(line,size,fp) != NULL){
-        //char rivi = line;
-        //printf("%s" ,rivi);
-        printf("%s" ,line);
-
-    }
-
-    fclose(fp);
-    printf("\n--loppu testi--\n");  //test 2
     return 0;
 }

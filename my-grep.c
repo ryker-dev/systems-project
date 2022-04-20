99db41e496aa520b1d90c9a1dd91d05bb90ccf38
JuhoSpoof <juho.spoof@kolumbus.fi>
2022-04-20 17:58:11 +0300

    muut ku argc=2 toimii fgets

--- my-grep.c
+++ my-grep.c
@@ -5,35 +5,58 @@
 
 int main(int argc, char const *argv[]){
 
-    if (argc < 2){
+    char line[256];
+    //char *line2 = NULL;
+    //size_t len = 0;
+
+    if (argc == 1){
         printf("my-grep: searchterm [file ...]\n");
-        exit(1); //??
-    }
+        exit(1); 
+    }/*
+    else if (argc == 2){
+        printf("2 argc\n");
+        //while ((read = getline(&line, &len, stream)) != -1) {
+        while (getline(&line2, &len, stdin) != NULL) {
+            printf("Retrieved line of length %zu :\n", read);
+            printf("%s", line);
+        }
 
-    int last_arg = argc;
-    printf("arg määrä = %d\n",last_arg);
-    //char filename[]="test.txt",line[200],search_string[]="foo";
-    char filename[256];
-    char search_string[256];
-    strcpy(file, argv[i]);
+    }*/
+    else{
 
-    FILE *fp = fopen(file, "r");
-    if (fp == NULL) {
-        printf("my-cat: cannot open file\n");
-        exit(1);
-    }
+        //int last_arg = argc; // poois?
 
-    char line[200];
-    fp=fopen(filename,"r");
-    if(!fp){
-        perror("could not find the file");
-        exit(0);
-    }
-    while ( fgets ( line, 200, fp ) != NULL ){ /* read a line */
-        if(strstr(line,search_string))
-        fputs ( line, stdout ); /* write the line */
-    }
+        //printf("arg määrä = %d\n",last_arg);
+        //char filename[]="test.txt",line[200],search_string[]="foo";
+        //char filename[256];
+        //char search_string[256];
+        //printf("%d\n",argc);
+        
+        for (int i = 2; i < argc; i++){
 
-    fclose (fp);
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL) {
+                printf("my-grep: cannot open file\n");
+                exit(1);
+            }
+
+            //char line[256];
+            /*
+            fp=fopen(filename,"r");
+            if(!fp){
+                perror("could not find the file");
+                exit(0);
+            }
+            */
+           
+            while ( fgets ( line, 256, fp ) != NULL ){ // getline?
+                //if(strstr(line,search_string))
+                if(strstr(line,argv[1]))
+                fputs ( line, stdout ); 
+            }
+
+            fclose(fp);
+        }
+    }
     return 0;
 }

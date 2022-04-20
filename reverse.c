5cbd528ee2d92b322c05ecee4554108bb1daa4cb
ryker-dev <pizzuti.dev@gmail.com>
2022-04-20 22:28:12 +0300

    bad reverse

--- reverse.c
+++ reverse.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* File opening and error handling */
 FILE *openFile(const char* name, char* mode) {
-    FILE *fp = fopen(name, "r");
+    FILE *fp = fopen(name, mode);
 
     if (fp == NULL) {
         fprintf(stderr, "error: cannot open file '%s'\n", name);
@@ -14,7 +15,39 @@ FILE *openFile(const char* name, char* mode) {
 }
 
 void reverse(FILE *input, FILE *output) {
-    /* Code here */
+    //int lengths = 10;
+    char *buffer;
+    char **lines;
+    size_t buffsize = 32;
+    lines = malloc(buffsize * sizeof(char*));
+
+/*      if ((lines = (char *)malloc(1 * sizeof(int))) == NULL) {
+        fprintf(stderr, "malloc failed");
+        exit(1);
+    } */
+
+    int length, index = 0;
+    lines[0] = NULL;
+    /* Method for allocation by anioss from stackoverflow.com */
+    while ((length = getline(&lines[index], &buffsize, input)) != -1) {
+/*         if (lines[length - 1] == '\n') {
+            lines[length - 1] = '\0';
+        }; */
+        printf("%s", lines[index]);
+        lines[index + 1] = NULL;
+        index++;
+/*         lines[index] = malloc(strlen(buffer) + 1);
+        strcpy(lines[index], buffer);
+        index++; */
+    }
+    
+    for (int i = index; i > -1; i--)
+    {
+/*         char *lastChar = lines[i][strlen(string)-1];
+        if (lastChar == '\n') {} */
+        printf("%s\n", lines[i]);
+    }
+
 }
 
 int main(int argc, char const *argv[])

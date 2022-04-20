0d93550b6cc92d06d681289224ad1c42dfe5c8d7
ryker-dev <pizzuti.dev@gmail.com>
2022-04-20 22:56:45 +0300

    output now going to a file

--- reverse.c
+++ reverse.c
@@ -21,29 +21,24 @@ void reverse(FILE *input, FILE *output) {
     size_t buffsize = 32;
     lines = malloc(buffsize * sizeof(char*));
 
-/*      if ((lines = (char *)malloc(1 * sizeof(int))) == NULL) {
-        fprintf(stderr, "malloc failed");
-        exit(1);
-    } */
-
-    int length, index = 0;
-    lines[0] = NULL;
     /* Method for array allocation by anioss from stackoverflow.com */
     /* MAN: Alternatively, before calling getline(), *lineptr can contain a pointer to a malloc(3)-allocated buffer *n bytes in size.
     If the buffer is not large enough to hold the line, getline() resizes it with realloc(3), updating *lineptr and *n as necessary. */
+    int length, index = 0;
+    lines[0] = NULL;
     while ((length = getline(&lines[index], &buffsize, input)) != -1) {
-        fprintf(stdout, "%s", lines[index]);
         lines[index + 1] = NULL;
         index++;
     }
-
     printf("\n");
+
     for (int i = index - 1; i > -1; i--)
     {
-        fprintf(stdout, "%s", lines[i]);
+        fprintf(output, "%s", lines[i]);
 
+        /* Take into account the possibility of a missing new line */
         if (lines[i][strlen(lines[i])-1] != '\n') {
-            fprintf(stdout, "\n");
+            fprintf(output, "\n");
         }
     }
 

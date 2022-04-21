6bc9e74c558f350be26081522b7e986b93e232f9
JuhoSpoof <juho.spoof@kolumbus.fi>
2022-04-21 21:02:48 +0300

    messages + grep while -> if

--- my-grep.c
+++ my-grep.c
@@ -11,32 +11,37 @@ int main(int argc, char const *argv[]){
     char *line = NULL;
     size_t len = 0;
 
+    // printing error if no arguments are given
     if (argc == 1){
         printf("my-grep: searchterm [file ...]\n");
         exit(1); 
     }
+    // searching from user input if only search parameter is given as argument
     else if (argc == 2){
-        while (getline(&line, &len, stdin) != -1) {
+        if (getline(&line, &len, stdin) != -1) {
             if(strstr(line,argv[1]))
             fputs(line, stdout); 
         }
     }
+    // searching one or multiple given files for search string and printing found lines
     else{
         for (int i = 2; i < argc; i++){
-
+            
+            //file opening and error handling
             FILE *fp = fopen(argv[i], "r");
             if (fp == NULL) {
                 printf("my-grep: cannot open file\n");
                 exit(1);
             }
 
+            //printing lines from file that match search
             while (getline(&line, &len, fp) != -1) {
                 if(strstr(line,argv[1]))
                 fputs(line, stdout); 
             }
-            free(line);
             fclose(fp);
         }
+        free(line); // getline() allocates memory so it has to be freed
     }
 
     return 0;
